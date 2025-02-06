3db0809828b6c3312726f65882c7c13ad0b43507
Pranav Shekhar <pranavshekhar00@gmail.com>
2025-02-06 10:23:38 +0530

    change ./utils/Store to ./utils/store because it is a case sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
 import { Provider } from "react-redux"
-import store from "./utils/Store"
+import store from "./utils/store"
 
 
 function App() {
